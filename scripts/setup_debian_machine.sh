5c9b274ccbf8f426df850112f48818bb39db355a
John Lukenoff <jlukenoff@gmail.com>
2023-12-17 13:15:27 -0800

    fix
    Signed-off-by: John Lukenoff <jlukenoff@gmail.com>

--- scripts/setup_debian_machine.sh
+++ scripts/setup_debian_machine.sh
@@ -24,7 +24,12 @@ else
 fi
 
 ## Make zsh the default for tmux
-sudo sh -c "echo 'set-option -g default-shell /bin/zsh' >> /etc/tmux.conf"
+set_tmux_default_shell="set-option -g default-shell /bin/zsh"
+if ! grep -qF "$set_tmux_default_shell" /etc/tmux.conf; then
+    sudo sh -c "echo 'set-option -g default-shell /bin/zsh' >> /etc/tmux.conf"
+else
+    echo "Tmux default shell already set"
+fi
 
 # Install a good vim config
 if [ -d ~/.vim_runtime ]; then
@@ -35,25 +40,21 @@ else
 fi
 
 # Set the path to the aliases file
-zsh_extras_path=$DIR/../configs/zshrc
-zsh_aliases_path=$DIR/../configs/aliases
-
-cat "$zsh_extras_path" >> ~/.zshrc
+use_zsh_extras="source $DIR/../configs/zshrc"
+use_zsh_aliases="source $DIR/../configs/aliases"
 
 # check if ~/.zshrc already containts the contents of $zsh_aliases_path, if not, append it
-if ! grep -qF "$zsh_extras_path" ~/.zshrc; then
-    echo "source $zsh_extras_path" >> ~/.zshrc
+if ! grep -qF "$use_zsh_extras" ~/.zshrc; then
+    echo "$use_zsh_extras" >> ~/.zshrc
 else
     echo "ZSH extras already present in ~/.zshrc"
 fi
 
-if ! grep -qF "$zsh_aliases_path" ~/.zsh_aliases; then
-    echo "source $zsh_aliases_path" >> ~/.zsh_aliases
+if ! grep -qF "$use_zsh_aliases" ~/.zshrc; then
+    echo "$use_zsh_aliases" >> ~/.zshrc
 else
-    echo "Aliases already present in ~/.zsh_aliases"
+    echo "ZSH aliases already present in ~/.zshrc"
 fi
-
-
 
 chsh $(whoami) -s $(which zsh)
 
